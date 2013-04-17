d900db6ded43d91e26ccc3a46202267ccbf41a94
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-04-17 16:00:56 -0400

    attempt session

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,5 +1,17 @@
 class SmsController < ApplicationController
   def create
     Sms.create(:incoming_number => params['From'], :content_received => params['Body'])
+    user = User.find_or_create_by_phone(params['From'])
+
+    session["counter"] ||= 0
+    sms_count = session["counter"]
+    if sms_count == 0
+      message = "Hello, thanks for the new message."
+    else
+      message = "Hello, thanks for message number #{sms_count + 1}"
+    end
+
+    session["counter"] += 1
+    Text.send_text_to(user, message)
   end
 end
