3e61cd1bf325780362460a1db184ebcf9e126c1d
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-04-17 15:48:52 -0400

    try again

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,5 +1,5 @@
 class SmsController < ApplicationController
   def create
-    Sms.create(:incoming_number => params['from'], :content_received => params['body'])
+    Sms.create(:incoming_number => params['From'], :content_received => params['Body'])
   end
 end
